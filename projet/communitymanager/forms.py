9ca997ce310d5e13a6f0f0f3b7a74226c98d8525
Faustine Douce <faustine.douce@student.isae-supaero.fr>
2021-05-18 11:41:28 +0200

    5.1 ok : connexion et déconnexion de l'utilisateur

---
+++ projet/communitymanager/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class ConnexionForm(forms.Form):
+    name = forms.CharField(label="Identifiant",required=False)
+    password = forms.CharField(label="Mot de Passe",max_length=100)
--- projet/communitymanager/views.py
+++ projet/communitymanager/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import ConnexionForm
+
+def accueil(request):
+    return render(request,'accueil.html',locals())
+
+
+def connexion(request):
+    """ Permet à l'utilisateur de se connecter"""
+
+    error = False
+    form = ConnexionForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data['name']
+        password = form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        if user and user.is_active:
+            login(request, user)
+            return redirect(accueil)
+
+        else:
+            error=True
+
+    else:
+        form = ConnexionForm()
+    return render(request, 'connexion.html', locals())
+
+def deconnexion(request):
+    logout(request)
+    return redirect(connexion)
+
+
